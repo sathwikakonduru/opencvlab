be0f2c148a1987879a560476ac9a20581ffac886
Sathwika konduru <11239A049@kanchiuniv.ac.in>
2026-02-02 16:33:58 +0530

    Add files via upload

---
+++ EX-3.py
@@ -0,0 +1,7 @@
+import cv2
+img = cv2.imread("image.jpg")
+gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+cv2.imshow("Original Image", img)
+cv2.imshow("Grayscale Image", gray)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
